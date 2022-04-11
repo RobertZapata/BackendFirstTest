142ff9bbdcba33b28465a29bb9ba75f0b4815675
RobertZapata <101352118+RobertZapata@users.noreply.github.com>
2022-04-10 20:00:40 -0500

    Initial commit

---
+++ BackendFirstTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace BackendFirstTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Campo de texto: ");
+            Console.WriteLine("Campo de texto sin caracteres especiales: " + RemoveEspecialCharacters(Console.ReadLine()));
+
+            static string RemoveEspecialCharacters(string text)
+            {
+                string emptyString = "NA";
+                string formatedText = Regex.Replace(text, @"[^\w\s _-]", string.Empty);
+                if (formatedText != String.Empty)
+                    return formatedText;
+                else return emptyString;
+            }
+        }
+    }
+}
